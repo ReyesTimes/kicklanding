7d9d0163a6bd7bcb5d3e439f7be965cc52fc6e56
juan.reyes <juan.reyes@interalia.net>
2018-06-21 13:12:20 -0500

    :hammer: add babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-    entry: './src/index.js'
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            }
+        ]
+    }
 };
